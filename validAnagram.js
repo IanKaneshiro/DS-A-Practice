1a81526d63634ae457d4f2025b4e16e8ee51f992
Ian <iandkaneshiro@gmail.com>
2024-08-01 06:19:15 -0700

    solved

---
+++ validAnagram.js
@@ -0,0 +1,22 @@
+isAnagram(s, t) {
+    const hashMap = {};
+
+    if(s.length !== t.length) return false;
+
+    for(let i = 0; i < s.length; i++) {
+        hashMap[s[i]] = (hashMap[s[i]] || 0) + 1;
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        if(hashMap[t[i]]) {
+            hashMap[t[i]]--
+        } else {
+            return false;
+        }
+    }
+
+    for(let val of Object.values(hashMap)) {
+        if(val !== 0) return false;
+    }
+    return true;
+}
