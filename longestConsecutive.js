54570889358e83fb8cf219c12ca94b497c0d19a5
Ian <iandkaneshiro@gmail.com>
2024-08-05 14:36:35 -0700

    fixed bug with function declaration

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -1,15 +1,15 @@
-longestConsecutive(nums) {
-    const numSet = new Set(nums);
-    let longest = 0;
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let longest = 0;
 
-    for (const n of numSet) {
-        if (!numSet.has(n - 1)) {
-            let length = 1;
-            while (numSet.has(n + length)) {
-                length++;
-            }
-            longest = Math.max(length, longest);
-        }
+  for (const n of numSet) {
+    if (!numSet.has(n - 1)) {
+      let length = 1;
+      while (numSet.has(n + length)) {
+        length++;
+      }
+      longest = Math.max(length, longest);
     }
-    return longest;
+  }
+  return longest;
 }
