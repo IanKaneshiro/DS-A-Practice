3cd32aa568b30fe7f4ce9278cd5d8c0a87165fdd
Ian <iandkaneshiro@gmail.com>
2024-08-08 18:14:18 -0700

    solution to linked-palindrome

--- mixed_recal.js
+++ mixed_recal.js
@@ -4,3 +4,13 @@ class Node {
     this.next = null;
   }
 }
+const linkedPalindrome = (head) => {
+  const values = [];
+  let current = head;
+
+  while (current) {
+    values.push(current.val);
+    current = current.next;
+  }
+  return values.join("") === values.reverse().join("");
+};
